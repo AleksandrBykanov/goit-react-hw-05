d1df95e18b5738b68ebe2270c79bce333f5bf5c0
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-08-22 23:35:50 +0300

    make reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import MoviesPage from './pages/MoviesPage/MoviesPage'
-import Navigation from './components/Navigation/Navigation'
-import MoviesDetailsPage from './pages/MoviesDetailsPage/MoviesDetailsPage'
-import MovieCast from './components/MovieCast/MovieCast'
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import Navigation from "./components/Navigation/Navigation";
+import MoviesDetailsPage from "./pages/MoviesDetailsPage/MoviesDetailsPage";
+import MovieCast from "./components/MovieCast/MovieCast";
+import MovieReviews from "./components/MovieReviews/MovieReviews";
 
-import './App.module.css'
+import "./App.module.css";
 
 function App() {
-
   return (
     <div>
       <Navigation />
       <main>
         <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/movies' element={<MoviesPage/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
           <Route path="/movies/:movieId" element={<MoviesDetailsPage />}>
-          <Route path="cast" element={<MovieCast />} />
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
           </Route>
         </Routes>
       </main>
       <footer></footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import css from "./MovieReviews.module.css";
+import Loader from "../../components/Loader/Loader";
+
+const MovieReviews = () => {
+  const [movies, setMovies] = useState(null);
+  const [loader, setLoader] = useState(false);
+
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function fetchReviews() {
+      try {
+        setLoader(true);
+        const url = `https://api.themoviedb.org/3/movie/${movieId}/reviews?language=en-US`;
+
+        const options = {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTA1NDYxZWZlMjFkNzljNGNjODUwMDgyNzZkNmJmYiIsIm5iZiI6MTcyNDE3NzU2Ny4yMjE4ODYsInN1YiI6IjY2YzFiZWE0YzM0MGY3MDllMGMyMWY3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qgD4TkBZHZj1C94QJw_7WDuP1JuznwefhaM80g6K4oo",
+          },
+        };
+        const { data } = await axios.get(url, options);
+        setMovies(data.results);
+      } catch (error) {
+        alert(JSON.stringify(error));
+      } finally {
+        setLoader(false);
+      }
+    }
+    fetchReviews();
+  }, [movieId]);
+  console.log(movies);
+  
+  return (
+    <>
+      <ul>
+        {Array.isArray(movies) > 0 ?
+          movies.map((item) => {
+            return (
+              <li key={item.id}>
+                <h1 className={css.h1}>{item.author}</h1>
+                <p className={css.p}>{item.content}</p>
+              </li>
+            );
+          }) : <p className={css.not}>We don`t have any reviews for this movie.</p> } 
+      </ul>
+      {loader && <Loader />}
+    </>
+  );
+};
+
+export default MovieReviews
